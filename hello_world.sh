c1a8edb1cbe2c21dbae5194e403d06b84ffdd481
mandlerm <amaya.melissa@gmail.com>
2019-09-05 19:46:20 -0500

    looping files

--- hello_world.sh
+++ hello_world.sh
@@ -28,53 +28,73 @@
 #   echo File exists
 # fi
 
-integer=4
+# integer=4
 
-if [[ $integer -lt 10 ]]
+# if [[ $integer -lt 10 ]]
+# then
+#   echo $integer is less than 10
+
+#   if [[ $integer -lt 5 ]]
+#   then
+#     echo $integer is also less than 5
+#   fi
+# fi
+
+
+# integer=15
+
+# if [[ $integer -lt 10 ]]
+# then
+#   echo $integer is less than 10
+# else
+#   echo $integer is not less than 10
+# fi
+
+
+# integer=15
+# if [[ $integer -lt 10 ]]
+# then
+#   echo $integer is less than 10
+# elif [[ $integer -gt 20 ]]
+#   then
+#     echo $integer is greater than 20
+#   else
+#     echo $integer is between 10 and 20
+#   fi
+
+
+# integer=15
+
+# if [[ $integer -gt 10 ]] && [[ $integer -lt 20 ]]
+# then
+#   echo $integer is between 10 and 20
+# fi
+
+# integer=7
+
+# if ! ([ $integer -eq 5 ]) || ! ([ $integer -eq 6 ])
+# then
+#   echo $integer is not 5 or 6
+# fi
+
+
+integer=8
+
+if [[ $integer = 5 ]]
 then
-  echo $integer is less than 10
-
-  if [[ $integer -lt 5 ]]
+  echo $integer is 5
+elif [[ $integer -gt 0 ]] && [[ $integer -lt 5 ]]
   then
-    echo $integer is also less than 5
-  fi
-fi
-
-
-integer=15
-
-if [[ $integer -lt 10 ]]
-then
-  echo $integer is less than 10
+  echo $integer is greater than zero and less than 5
 else
-  echo $integer is not less than 10
+  echo $integer i have no idea
 fi
 
+integer=8
 
-integer=15
-if [[ $integer -lt 10 ]]
+if !([ $integer -le 2 ])
 then
-  echo $integer is less than 10
-elif [[ $integer -gt 20 ]]
-  then
-    echo $integer is greater than 20
-  else
-    echo $integer is between 10 and 20
-  fi
-
-
-integer=15
-
-if [[ $integer -gt 10 ]] && [[ $integer -lt 20 ]]
-then
-  echo $integer is between 10 and 20
-fi
-
-integer=7
-
-if ! ([ $integer -eq 5 ]) || ! ([ $integer -eq 6 ])
-then
-  echo $integer is not 5 or 6
+  echo $integer is greater than 2
 fi
 
 
