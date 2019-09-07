64f96d445a0a2142d00f27d4626993461681d5fa
mandlerm <amaya.melissa@gmail.com>
2019-09-07 07:02:51 -0500

    working on dynamically obtaining the length of the request

--- simple_http_server/http_server.sh
+++ simple_http_server/http_server.sh
@@ -1,24 +1,38 @@
 #!/usr/local/bin/bash
 
 function server () {
-
   while true
+  do
+    message_arr=()
+    check=true
+    cat file1
+    while $check
     do
-      read  message path version
-      if [[ $message = "GET" ]]
+      read line
+      cat file1 - > $line
+      message_arr+=($line)
+      if [[ "${#line}" -le 1 ]]
       then
-        if [[ -f "./www/$path.html" ]]; then
-          echo -ne "HTTP/1.1 200 OK\r\n\r\n"; cat "./www/$path.html"
-        else
-        echo -ne "HTTP/1.1 404 Not Found\r\n\r\n"
-        fi
-
-      else
-      echo -ne "HTTP/1.1 400 Bad Request\r\n\r\n"
+        check=false
       fi
     done
+    method=${message_arr[0]}
+    path=${message_arr[1]}
+    file_length=wc -c file1
+    if [[ $method = 'GET' ]]
+    then
+      if [[ -f "./www/$path" ]]
+      then
+        echo -ne "HTTP/1.1 200 OK\r\n\r\nContent-Type: text/html; charset=utf-8\r\nContent-Length: $file_length\r\n"; cat "./www/$path"
+      else
+        echo 'HTTP/1.1 404 Not Found\r\nContent-Length: 0\r\n\r\n'
+      fi
+    else
+      echo -ne 'HTTP/1.1 400 Bad Request\r\nContent-Length: 0\r\n\r\n'
+    fi
+  done
 }
 
 coproc SERVER_PROCESS { server; }
 
-nc -lv 2345 <&${SERVER_PROCESS[0]} >&${SERVER_PROCESS[1]}
+nc -lkv 2345 <&${SERVER_PROCESS[0]} >&${SERVER_PROCESS[1]}
